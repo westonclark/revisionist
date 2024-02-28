7fa01ef5ad4d885619a3ca76fcdbaf7ecaafa533
Weston Clark <westoclark@gmail.com>
2024-02-28 11:34:24 -0800

    add components

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import LampDemo from "../components/ui/lamp.tsx"
 
 export default function Home() {
   return (
     <main className="">
-      <p>Helle Revisionist</p>
+      <h1>Revisions.com</h1>
     </main>
   );
 }
